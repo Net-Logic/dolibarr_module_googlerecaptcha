197a63dd2340003c641f58675dce0d614f448c39
Frédéric FRANCE <frederic.france@free.fr>
2023-01-26 13:58:53 +0100

    clean code

--- core/login/functions_googlerecaptcha.php
+++ core/login/functions_googlerecaptcha.php
@@ -27,55 +27,55 @@
  */
 function check_user_password_googlerecaptcha($usertotest, $passwordtotest, $entitytotest)
 {
-    global $conf;
+	global $conf;
 
-    // recaptcha 3
-    require_once DOL_DOCUMENT_ROOT.'/core/login/functions_dolibarr.php';
-    // We first check if user & password are OK
-    dol_syslog("functions_googlerecaptcha::check_user_password_googlerecaptcha", LOG_DEBUG);
-    if (check_user_password_dolibarr($usertotest, $passwordtotest, $entitytotest) == '') {
-        // if test fails then no login...
-        return '';
-    }
+	// recaptcha 3
+	require_once DOL_DOCUMENT_ROOT . '/core/login/functions_dolibarr.php';
+	// We first check if user & password are OK
+	dol_syslog("functions_googlerecaptcha::check_user_password_googlerecaptcha", LOG_DEBUG);
+	if (check_user_password_dolibarr($usertotest, $passwordtotest, $entitytotest) == '') {
+		// if test fails then no login...
+		return '';
+	}
 
-    // Force master entity in transversal mode
-    $entity = $entitytotest;
-    if (!empty($conf->multicompany->enabled) && !empty($conf->multicompany->transverse_mode)) {
-        $entity = 1;
-    }
-    dol_syslog("functions_googlerecaptcha::check_googlerecaptcha", LOG_DEBUG);
-    $captcha = GETPOST('g-recaptcha-response', 'san_alpha');
-    //$ip = $_SERVER['REMOTE_ADDR'];
+	// Force master entity in transversal mode
+	$entity = $entitytotest;
+	if (!empty($conf->multicompany->enabled) && !empty($conf->multicompany->transverse_mode)) {
+		$entity = 1;
+	}
+	dol_syslog("functions_googlerecaptcha::check_googlerecaptcha", LOG_DEBUG);
+	$captcha = GETPOST('g-recaptcha-response', 'san_alpha');
+	//$ip = $_SERVER['REMOTE_ADDR'];
 
-    // post request to server
-    $url = 'https://www.google.com/recaptcha/api/siteverify';
-    $data = array(
-        'secret' => $conf->global->GOOGLERECAPTCHA_SERVER_KEY,
-        'response' => $captcha,
-    );
-    $options = array(
-        'http' => array(
-            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-            'method'  => 'POST',
-            'content' => http_build_query($data),
-        )
-    );
-    $context  = stream_context_create($options);
-    $response = file_get_contents($url, false, $context);
-    $responseKeys = json_decode($response, true);
-    // {
-    //     "success": true|false,      // whether this request was a valid reCAPTCHA token for your site
-    //     "score": number             // the score for this request (0.0 - 1.0)
-    //     "action": string            // the action name for this request (important to verify)
-    //     "challenge_ts": timestamp,  // timestamp of the challenge load (ISO format yyyy-MM-dd'T'HH:mm:ssZZ)
-    //     "hostname": string,         // the hostname of the site where the reCAPTCHA was solved
-    //     "error-codes": [...]        // optional
-    // }
-    if ($responseKeys['success'] === true && (float) $responseKeys['score'] > 0.7 && $responseKeys['action'] == 'login') {
-        // Now the user is authenticated
-        // setEventMessage('Score recaptcha '. (string) $responseKeys['score']);
-        return $usertotest;
-    }
-    $_SESSION["dol_loginmesg"] = 'No login for now... ';
-    return '';
+	// post request to server
+	$url = 'https://www.google.com/recaptcha/api/siteverify';
+	$data = array(
+		'secret' => $conf->global->GOOGLERECAPTCHA_SERVER_KEY,
+		'response' => $captcha,
+	);
+	$options = array(
+		'http' => array(
+			'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+			'method'  => 'POST',
+			'content' => http_build_query($data),
+		)
+	);
+	$context  = stream_context_create($options);
+	$response = file_get_contents($url, false, $context);
+	$responseKeys = json_decode($response, true);
+	// {
+	//     "success": true|false,      // whether this request was a valid reCAPTCHA token for your site
+	//     "score": number             // the score for this request (0.0 - 1.0)
+	//     "action": string            // the action name for this request (important to verify)
+	//     "challenge_ts": timestamp,  // timestamp of the challenge load (ISO format yyyy-MM-dd'T'HH:mm:ssZZ)
+	//     "hostname": string,         // the hostname of the site where the reCAPTCHA was solved
+	//     "error-codes": [...]        // optional
+	// }
+	if ($responseKeys['success'] === true && (float) $responseKeys['score'] > 0.7 && $responseKeys['action'] == 'login') {
+		// Now the user is authenticated
+		// setEventMessage('Score recaptcha '. (string) $responseKeys['score']);
+		return $usertotest;
+	}
+	$_SESSION["dol_loginmesg"] = 'No login for now... ';
+	return '';
 }
